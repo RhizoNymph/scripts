7cd641ae7031969523e1d90591a1442393458b2a
Nymph <82485126+QuantNymph@users.noreply.github.com>
2023-10-12 21:00:43 -0700

    Fix tmux for reth in both

--- reth_setup.sh
+++ reth_setup.sh
@@ -32,7 +32,7 @@ mkdir $TARGET_DIR/lighthouse/data
 
 echo -e "alias lh='lighthouse bn     --checkpoint-sync-url https://mainnet.checkpoint.sigp.io     --execution-endpoint http://localhost:8551     --execution-jwt $TARGET_DIR/jwt.hex   --disable-deposit-contract-sync'" >> $TARGET_DIR/.bashrc
 echo -e "alias r='reth node --datadir $TARGET_DIR/reth/data/ --metrics 127.0.0.1:9001 --http --http.addr 0.0.0.0 --http.api All --ws  --authrpc.jwtsecret $TARGET_DIR/jwt.hex'" >> $TARGET_DIR/.bashrc
-echo -e "alias both='tmux new-session -d -s lh bash -i -c "lh" \; new-session -d -s r bash -i -c "r"'" >> $TARGET_DIR/.bashrc
+echo -e "alias both='tmux new-session -d -s lh bash -i -c "lh" \; tmux new-session -d -s r bash -i -c "r"'" >> $TARGET_DIR/.bashrc
 echo -e "both" >> $TARGET_DIR/.bashrc
 source $TARGET_DIR/.bashrc
 
