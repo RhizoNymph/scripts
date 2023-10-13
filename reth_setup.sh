d387a6b1de777ff8e6d9008b0d5cc94ebe4f3173
Nymph <82485126+QuantNymph@users.noreply.github.com>
2023-10-12 21:07:22 -0700

    Update reth_setup.sh
    change target dir to ~ for bashrc (artifact of find and replace ~ for target dir)

--- reth_setup.sh
+++ reth_setup.sh
@@ -30,11 +30,11 @@ mkdir $TARGET_DIR/reth/data
 mkdir $TARGET_DIR/lighthouse
 mkdir $TARGET_DIR/lighthouse/data
 
-echo -e "alias lh='lighthouse bn     --checkpoint-sync-url https://mainnet.checkpoint.sigp.io     --execution-endpoint http://localhost:8551     --execution-jwt $TARGET_DIR/jwt.hex   --disable-deposit-contract-sync'" >> $TARGET_DIR/.bashrc
-echo -e "alias r='reth node --datadir $TARGET_DIR/reth/data/ --metrics 127.0.0.1:9001 --http --http.addr 0.0.0.0 --http.api All --ws  --authrpc.jwtsecret $TARGET_DIR/jwt.hex'" >> $TARGET_DIR/.bashrc
-echo -e "alias both='tmux new-session -d -s lh bash -i -c \"lh\"; tmux new-session -d -s r bash -i -c \"r\"'" >> $TARGET_DIR/.bashrc
-echo -e "both" >> $TARGET_DIR/.bashrc
-source $TARGET_DIR/.bashrc
+echo -e "alias lh='lighthouse bn     --checkpoint-sync-url https://mainnet.checkpoint.sigp.io     --execution-endpoint http://localhost:8551     --execution-jwt $TARGET_DIR/jwt.hex   --disable-deposit-contract-sync'" >> ~/.bashrc
+echo -e "alias r='reth node --datadir $TARGET_DIR/reth/data/ --metrics 127.0.0.1:9001 --http --http.addr 0.0.0.0 --http.api All --ws  --authrpc.jwtsecret $TARGET_DIR/jwt.hex'" >> ~/.bashrc
+echo -e "alias both='tmux new-session -d -s lh bash -i -c \"lh\"; tmux new-session -d -s r bash -i -c \"r\"'" >> ~/.bashrc
+echo -e "both" >> ~/.bashrc
+source ~/.bashrc
 
 openssl rand -hex 32 | tr -d "\n" | sudo tee $TARGET_DIR/jwt.hex
 
